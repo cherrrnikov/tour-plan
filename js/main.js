ab16cc72bee05c72e7bde6a9d18708525c8f651c
cherrrnikov <chernikoviam@gmail.com>
2020-10-05 20:16:06 +0300

    fixed: adaptive for modal window

--- js/main.js
+++ js/main.js
@@ -1,101 +1,134 @@
-var hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  effect: "coverflow",
-  // Подключение клавиатуры
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  },
-});
+$(document).ready(function () {
+  //Параллакс
+  $window = $(window);
+  $('div[data-type="background"]').each(function () {
+    var $bgobj = $(this);
+    $(window).scroll(function () {
+      var yPos = -($window.scrollTop() / $bgobj.data("speed"));
+      var coords = "50% " + yPos + "px";
+      $bgobj.css({ backgroundPosition: coords });
+    });
+  });
 
-var reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-  // Подключение клавиатуры
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  },
-});
+  var hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    effect: "coverflow",
+    // Подключение клавиатуры
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+  });
 
-// Добавление карты
-ymaps.ready(function () {
-  var myMap = new ymaps.Map(
-      "map",
-      {
-        center: [7.838196, 98.298813],
-        zoom: 15,
-      },
-      {
-        searchControlProvider: "yandex#search",
-      }
-    ),
-    // Создаём макет содержимого.
-    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-    ),
-    myPlacemark = new ymaps.Placemark(
-      myMap.getCenter(),
-      {
-        hintContent: "Собственный значок метки",
-        balloonContent: "Это красивая метка",
-      },
-      {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: "default#image",
-        // Своё изображение иконки метки.
-        iconImageHref: "img/point.png",
-        // Размеры метки.
-        iconImageSize: [42, 42],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-5, -38],
-      }
-    ),
-    myPlacemarkWithContent = new ymaps.Placemark(
-      [55.661574, 37.573856],
-      {
-        hintContent: "Собственный значок метки с контентом",
-        balloonContent: "А эта — новогодняя",
-        iconContent: "12",
-      },
-      {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: "default#imageWithContent",
-        // Своё изображение иконки метки.
-        iconImageHref: "img/point.png",
-        // Размеры метки.
-        iconImageSize: [48, 48],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-24, -24],
-        // Смещение слоя с содержимым относительно слоя с картинкой.
-        iconContentOffset: [15, 15],
-        // Макет содержимого.
-        iconContentLayout: MyIconContentLayout,
-      }
-    );
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+    // Подключение клавиатуры
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+  });
 
-  myMap.geoObjects.add(myPlacemark).add(myPlacemarkWithContent);
-});
+  // Добавление карты
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map(
+        "map",
+        {
+          center: [7.838196, 98.298813],
+          zoom: 15,
+        },
+        {
+          searchControlProvider: "yandex#search",
+        }
+      ),
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+      myPlacemark = new ymaps.Placemark(
+        myMap.getCenter(),
+        {
+          hintContent: "Собственный значок метки",
+          balloonContent: "Это красивая метка",
+        },
+        {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: "default#image",
+          // Своё изображение иконки метки.
+          iconImageHref: "img/point.png",
+          // Размеры метки.
+          iconImageSize: [42, 42],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-5, -38],
+        }
+      ),
+      myPlacemarkWithContent = new ymaps.Placemark(
+        [55.661574, 37.573856],
+        {
+          hintContent: "Собственный значок метки с контентом",
+          balloonContent: "А эта — новогодняя",
+          iconContent: "12",
+        },
+        {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: "default#imageWithContent",
+          // Своё изображение иконки метки.
+          iconImageHref: "img/point.png",
+          // Размеры метки.
+          iconImageSize: [48, 48],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-24, -24],
+          // Смещение слоя с содержимым относительно слоя с картинкой.
+          iconContentOffset: [15, 15],
+          // Макет содержимого.
+          iconContentLayout: MyIconContentLayout,
+        }
+      );
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("Клик по кнопке меню");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
+    myMap.geoObjects.add(myPlacemark).add(myPlacemarkWithContent);
+  });
+
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("Клик по кнопке меню");
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
