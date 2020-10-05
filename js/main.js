d388d74c3ce371838110ae8fb4de659352ee6b62
cherrrnikov <chernikoviam@gmail.com>
2020-10-05 21:50:05 +0300

    fixed: email form

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
 $(document).ready(function () {
-  //Параллакс
-  $window = $(window);
-  $('div[data-type="background"]').each(function () {
-    var $bgobj = $(this);
-    $(window).scroll(function () {
-      var yPos = -($window.scrollTop() / $bgobj.data("speed"));
-      var coords = "50% " + yPos + "px";
-      $bgobj.css({ backgroundPosition: coords });
-    });
-  });
-
   var hotelSlider = new Swiper(".hotel-slider", {
     // Optional parameters
     loop: true,
--- send.php
+++ send.php
@@ -8,72 +8,22 @@ require 'phpmailer/Exception.php';
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-
-// Формирование самого письма
-$title = "Новое обращение Best Tour Plan";
-$body = "
-<h2>Новое обращение</h2>
-<b>Имя:</b> $name<br>
-<b>Телефон:</b> $phone<br><br>
-<b>Сообщение:</b><br>$message
-";
-
-// Настройки PHPMailer
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-try {
-    $mail->isSMTP();   
-    $mail->CharSet = "UTF-8";
-    $mail->SMTPAuth   = true;
-    //$mail->SMTPDebug = 2; 
-    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
-
-    // Настройки вашей почты
-    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-    $mail->Username   = 'chernikoviam@gmail.com'; // Логин на почте
-    $mail->Password   = 'itmedusa_628'; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
-    $mail->setFrom('chernikoviam@gmail.com', 'Igor Chernikov'); // Адрес самой почты и имя отправителя
-
-    // Получатель письма
-    $mail->addAddress('hiiamdiv@yandex.ru');  
-
-    // Отправка сообщения
-    $mail->isHTML(true);
-    $mail->Subject = $title;
-    $mail->Body = $body;    
-    
-// Проверяем отравленность сообщения
-if ($mail->send()) {$result = "success";} 
-else {$result = "error";}
-
-} catch (Exception $e) {
-    $result = "error";
-    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
-}
-
-// Отображение результата
-header("Location: thankyou.html");
-?>
-
-<!-- НАСТРОЙКА ПОЧТЫ ДЛЯ NEWSLETTER
-
-<?php
-// Файлы phpmailer
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
-require 'phpmailer/Exception.php';
-
-// Переменные, которые отправляет пользователь
 $email = $_POST['email'];
 
-
 // Формирование самого письма
 $title = "Новое обращение Best Tour Plan";
-$body = "
-<h2>Новое обращение</h2>
-<b>Почта для рассылки:</b> $email<br>
-";
+if ($email) {
+    $body = "
+      <h2>Новое сообщение</h2>
+      <b>Почта:</b><br>$email
+    ";
+} else {
+    $body = "
+      <h2>Новое сообщение от </h2>$name<br>
+      <b>Телефон:</b> $phone<br>    
+      <b>Сообщение:</b><br>$message
+    ";
+}
 
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
@@ -85,12 +35,12 @@ try {
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
     // Настройки вашей почты
-    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-    $mail->Username   = 'chernikoviam@gmail.com'; // Логин на почте
-    $mail->Password   = 'ksdjhfgkljdfnb'; // Пароль на почте
+    $mail->Host       = 'smtp.yandex.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'zhekadolgovoi@yandex.com'; // Логин на почте
+    $mail->Password   = '05102020__zheka'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
-    $mail->setFrom('chernikoviam@gmail.com', 'Igor Chernikov'); // Адрес самой почты и имя отправителя
+    $mail->setFrom('zhekadolgovoi@yandex.com', 'Жека Долговой'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
     $mail->addAddress('hiiamdiv@yandex.ru');  
@@ -111,3 +61,5 @@ else {$result = "error";}
 
 // Отображение результата
 header("Location: thankyou.html");
+
+
